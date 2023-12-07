1f97bcb2fb559cf73dc907829fd75ddb2d3938a8
Ben <GRA230096@spengergasse.at>
2023-12-07 13:55:27 +0100

    rechner

---
+++ Rechner/script.js
@@ -0,0 +1,16 @@
+class Calculator {
+    constructor(beforeTextElement, nowTextElement) {
+    this.beforeTextElement = beforeTextElement
+    this.nowTextElement = nowTextElement
+    this.clear()
+}
+clear() {
+    this.before = ' '
+    this.now = ' '
+    this.operation = undefined
+}
+
+
+
+
+}
