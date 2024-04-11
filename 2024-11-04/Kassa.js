fd8b8950fcc337173db70051fb6e7458c242810c
Ben <GRA230096@spengergasse.at>
2024-04-11 14:14:45 +0200

    push

---
+++ 2024-11-04/Kassa.js
@@ -0,0 +1,11 @@
+class Kassa {
+  constructor(artikel, betrag) {
+    this.artikel = artikel;
+    this.bertag = betrag;
+  }
+  scannen(artikel) {}
+  bezahlen(betrag) {}
+  toString() {}
+}
+
+console.log(this.lorem.sentence({ min: 3, max: 5 }));
