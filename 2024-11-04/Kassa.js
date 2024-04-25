61bb54d83c762b5d7a61d8866a1255682d40fbf3
Ben <GRA230096@spengergasse.at>
2024-04-25 14:11:43 +0200

    Kassa betrug

--- 2024-11-04/Kassa.js
+++ 2024-11-04/Kassa.js
@@ -1,11 +1,22 @@
 class Kassa {
-  constructor(artikel, betrag) {
-    this.artikel = artikel;
-    this.bertag = betrag;
+  #artikel;
+  #geldLade;
+  constructor(betrag) {
+    this.#artikel = [];
+    this.#geldLade = geldLade;
   }
-  scannen(artikel) {}
-  bezahlen(betrag) {}
+  scannen(artikel) {
+    this.#artikel.push(artikel);
+  }
+  get offen() {
+    let betrag = 0;
+    this.#artikel.forEach((a) => (betrag += a.betrag));
+  }
+  bezahlen(geld) {
+    if (geld < this.offen) {
+      throw new Error("Zu wenig Geld");
+    }
+    const retour = geld - this.offen;
+  }
   toString() {}
 }
-
-console.log(this.lorem.sentence({ min: 3, max: 5 }));
